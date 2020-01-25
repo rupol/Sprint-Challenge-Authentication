ba67c8cedc10b5c66a403a0e04b78a1dabf3e20f
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-24 18:36:05 -0700

    register and login endpoints and tests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,59 @@
-const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authenticate = require("./authenticate-middleware");
+const usersModel = require("./users-model");
+const secrets = require("../config/secrets");
 
-router.post('/register', (req, res) => {
+const router = require("express").Router();
+
+router.post("/register", async (req, res, next) => {
   // implement registration
+  try {
+    const user = await usersModel.add(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", async (req, res, next) => {
   // implement login
+  try {
+    const { username, password } = req.body;
+    const user = await usersModel.getBy({ username }).first();
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (user && passwordValid) {
+      const token = signToken(user);
+
+      res.status(200).json({
+        message: "Successfully logged in",
+        user: user.id,
+        token
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid credentials, please check your username and password"
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
+
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const secret = secrets.jwt;
+
+  const options = {
+    expiresIn: "7d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
---
+++ auth/auth-router.spec.js
@@ -0,0 +1,34 @@
+const supertest = require("supertest");
+const server = require("../api/server");
+const db = require("../database/dbConfig");
+
+test("POST /register - register user route", async () => {
+  db.seed.run();
+  const res = await supertest(server)
+    .post("/api/auth/register")
+    .send({ username: "ruth", password: "123abc" });
+
+  // does it return the expected status code?
+  expect(res.status).toBe(201);
+
+  // does it return the expected data format?
+  expect(res.type).toBe("application/json");
+
+  // does it return the expected data?
+  expect(res.body.username).toMatch(/ruth/i);
+});
+
+test("POST /login - login user route", async () => {
+  const res = await supertest(server)
+    .post("/api/auth/login")
+    .send({ username: "ruth", password: "123abc" });
+
+  // does it return the expected status code?
+  expect(res.status).toBe(200);
+
+  // does it return the expected data format?
+  expect(res.type).toBe("application/json");
+
+  // does it return the expected data?
+  expect(res.body.message).toMatch(/successfully logged in/i);
+});
